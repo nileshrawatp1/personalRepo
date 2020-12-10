544dfee719898fbdf2f7736dd28c27b840273aab
nileshrawatp1 <nileshrawatp1@gmail.com>
2020-12-10 21:21:00 +0530

    arrays

---
+++ practice/php/arrays/array_Concatinate.php
@@ -0,0 +1,10 @@
+<?php
+
+$cars = array('BMW', 'Audi', 'Tata');
+echo "$cars[1] <br> $cars[2] <br>";
+
+// ! Another way of creating Arrays
+$peoples = ['Radhika', 'Akash', 'Nilesh'];
+echo "$peoples[0]<br>$peoples[2]";
+
+?>
