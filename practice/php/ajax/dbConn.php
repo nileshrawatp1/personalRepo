71816a13b99d7e1ded80a77011f70a0b7bd1216e
nileshrawatp1 <nileshrawatp1@gmail.com>
2021-02-20 22:37:37 +0530

    To Get Full URL Added

--- practice/php/ajax/dbConn.php
+++ practice/php/ajax/dbConn.php
@@ -1,5 +1,10 @@
 <?php
 
+// ! To get the full URL of the host
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+// OUTPUT ==>> https://nilesh.ameyo.net:8785/practice/php/loginPage/includes/dbh.inc.php
+
 $serverName = "nilesh.ameyo.net";
 $username = "postgres";
 $password = "postgres";
